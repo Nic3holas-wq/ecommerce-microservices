4b79654c2c28bcca4c4641f1e25608a653161965
Nicholas <nicholasmurimi254@gmail.com>
2026-07-18 12:24:20 +0300

    resolve authentication issue on order service

---
+++ services/order/src/main/java/com/nicko/ecommerce/config/FeignConfig.java
@@ -0,0 +1,25 @@
+package com.nicko.ecommerce.config;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes != null) {
+                String authorizationHeader = attributes.getRequest().getHeader(HttpHeaders.AUTHORIZATION);
+                if (authorizationHeader != null) {
+                    requestTemplate.header(HttpHeaders.AUTHORIZATION, authorizationHeader);
+                }
+            }
+        };
+    }
+}
--- services/order/src/main/java/com/nicko/ecommerce/config/RestTemplateConfig.java
+++ services/order/src/main/java/com/nicko/ecommerce/config/RestTemplateConfig.java
@@ -2,13 +2,36 @@ package com.nicko.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+        if (interceptors == null) {
+            interceptors = new ArrayList<>();
+        }
+        interceptors.add((request, body, execution) -> {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes != null) {
+                String authorizationHeader = attributes.getRequest().getHeader(HttpHeaders.AUTHORIZATION);
+                if (authorizationHeader != null) {
+                    request.getHeaders().set(HttpHeaders.AUTHORIZATION, authorizationHeader);
+                }
+            }
+            return execution.execute(request, body);
+        });
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate;
     }
 }
